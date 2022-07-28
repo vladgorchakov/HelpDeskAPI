04bbf39ba44ca99f2db6645ea5fe6d5f09fbd566
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-28 13:16:35 +0200

    create 'Status' model

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    title = models.CharField(max_length=20, unique=True)
+    description = models.TextField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def __int__(self):
+        return self.pk
