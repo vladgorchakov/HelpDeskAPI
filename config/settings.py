37bdc2c32bd407c3f780ee96cfe82e2b82ac16c3
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-28 12:52:59 +0200

    connect postges db to my project

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'helpdeskdb',
+        'USER': 'helpdeskdbadmin',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
