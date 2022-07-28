357a61606f78e6368ff15de158f29be2d6fa9616
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-28 14:14:48 +0200

    create tests for 'Status' model

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # installed apps
-    # created apps
+    # local apps
     'helpdesk.apps.HelpdeskConfig',
 ]
 
--- helpdesk/tests.py
+++ helpdesk/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from helpdesk.models import Status
 
-# Create your tests here.
+
+class StatusModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Status.objects.create(title='add status', description='add description')
+
+    def test_title_content(self):
+        status = Status.objects.get(id=1)
+        expected_object_name = f'{status.title}'
+        self.assertEqual(expected_object_name, 'add status')
+
+    def test_description_content(self):
+        status = Status.objects.get(id=1)
+        expected_object_name = f'{status.description}'
+        self.assertEqual(expected_object_name, 'add description')
