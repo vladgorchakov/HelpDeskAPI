c173835664ac61836397d151be1809ab112f5e84
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-07-29 12:15:07 +0200

    Install DRF

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # installed apps
+    'rest_framework',
     # local apps
     'helpdesk.apps.HelpdeskConfig',
 ]
