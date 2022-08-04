07c9cf7b8ee12b4f317493bdabeac3d914abb7ca
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-04 23:44:56 +0200

    Install djouser and add token auth

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # installed apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     # local apps
     'helpdesk.apps.HelpdeskConfig',
     'api.apps.ApiConfig',
@@ -130,3 +132,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+]
+}
