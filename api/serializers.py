18afcfb7b08285e40110fc5a1b17087d8621ce32
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-04 18:09:50 +0200

    create validators for Message serializer

--- api/serializers.py
+++ api/serializers.py
@@ -4,9 +4,24 @@ from helpdesk import models
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    # ticket = serializers.SlugRelatedField(slug_field='title',
-    #                                       queryset=models.Ticket.objects.all()
-    #                                       )
+    ticket = serializers.SlugRelatedField(slug_field='id',
+                                          queryset=models.Ticket.objects.all()
+                                          )
+    past_message = serializers.SlugRelatedField(slug_field='id',
+                                                queryset=models.Message.objects.all()
+                                                )
+
+    def validate(self, value):
+        user_id = self.context['request'].user.id
+        ticket_id = self.context['request'].data['ticket']
+        past_message_id = self.context['request'].data['past_message']
+
+        ticket = models.Ticket.objects.get(pk=ticket_id)
+        if not ticket.user.pk == user_id:
+            raise serializers.ValidationError(
+                'User is not owner of this ticket'
+            )
+        return value
 
     class Meta:
         model = models.Message
