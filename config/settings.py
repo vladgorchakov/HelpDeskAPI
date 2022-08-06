4a13041978fe989f413247723d5a250b1aec32a4
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-06 03:33:33 +0200

    Set EMAIL_BACKEND as console

--- config/settings.py
+++ config/settings.py
@@ -184,3 +184,5 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
