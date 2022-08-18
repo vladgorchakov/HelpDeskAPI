798657ae0890ac6979e1ba310030177ebbaf7e00
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-18 15:12:32 +0200

    Del empty files and comments

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from helpdesk.models import Status, Ticket, Message
+from helpdesk.models import Ticket, Message
 
 
-admin.site.register(Status)
 admin.site.register(Ticket)
 admin.site.register(Message)
