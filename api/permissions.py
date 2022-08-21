9378ee8ebe6456341e635534905e30adf27c9184
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-21 20:20:39 +0200

    Rename IsTicketAuthorOrStaff to IsAuthorOrStaff

--- api/permissions.py
+++ api/permissions.py
@@ -3,14 +3,28 @@ from helpdesk.models import Ticket
 from django.core.exceptions import ObjectDoesNotExist
 
 
-class IsMessageAuthor(permissions.BasePermission):
+class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.sender == request.user
+        return obj.user == request.user
 
 
 class IsAuthorOrStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user.is_staff
+
+
+class IsTicketAuthorOrStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            ticket_id = request.data.get('ticket', None)
+            if ticket_id:
+                try:
+                    return bool(request.user == Ticket.objects.get(pk=ticket_id).user
+                                or request.user.is_staff)
+                except ObjectDoesNotExist:
+                    return True
+
+        return True
 
 
 class MessagePermissions(permissions.BasePermission):
--- api/service.py
+++ api/service.py
@@ -5,8 +5,8 @@ import config.settings
 
 def send(user_email, title, status):
     send_mail(
-        subject='Your task status changed!',
-        message=f'Ticket: {title} | Status: {status}',
+        subject=f'Your ticket ({title}) changed status',
+        message=f'Status: {status}',
         from_email=config.settings.EMAIL_ADDRESS,
         recipient_list=[user_email],
         fail_silently=False
