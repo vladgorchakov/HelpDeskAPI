7a8cef9457bf56b5ea3191d403052426d6ed3c74
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-21 19:15:16 +0200

    Refactored MessagePermissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,8 +1,4 @@
 from rest_framework import permissions
-from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import SAFE_METHODS
-from rest_framework.response import Response
-
 from helpdesk.models import Ticket
 from django.core.exceptions import ObjectDoesNotExist
 
