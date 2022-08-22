533684e0afd86d66806e14821f1681a67bcbe48e
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-08-22 09:57:05 +0200

    rename variable EMAIL_ADDRESS

--- api/service.py
+++ api/service.py
@@ -1,13 +1,12 @@
 from django.core.mail import send_mail
-
-import config.settings
+from config.settings import EMAIL_ADDRESS as email_address
 
 
 def send(user_email, title, status):
     send_mail(
         subject=f'Your ticket ({title}) changed status',
         message=f'Status: {status}',
-        from_email=config.settings.EMAIL_ADDRESS,
+        from_email=email_address,
         recipient_list=[user_email],
         fail_silently=False
     )
